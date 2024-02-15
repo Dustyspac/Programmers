88c613f7a1c0f8f684ded92753be68170ad90953
Dustyspac <158263919+Dustyspac@users.noreply.github.com>
2024-02-15 12:55:59 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.75 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,10 +1,10 @@
 function solution(n, m) {
-    let answer = [];
     let maxNum = Math.max(n,m);
     let minNum = Math.min(n,m);
 
 function gcd(minNum, maxNum){
-    return(minNum % maxNum) === 0 ? maxNum : gcd(maxNum, minNum % maxNum)
+    console.log(minNum, maxNum)
+    return(maxNum % minNum) === 0 ? minNum : gcd(maxNum % minNum, minNum)
 }
 
 function lcm(minNum, maxNum){
@@ -12,5 +12,5 @@ function lcm(minNum, maxNum){
 }
 
 return [gcd(n, m), lcm(n, m)];
-    
+
 }
